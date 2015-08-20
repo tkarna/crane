414bae6bb226d7311a3f944bcf7a3a92cc525b2c
Tuomas Karna <tuomas.karna@gmail.com>
2015-08-20 11:42:45 -0700

    moved scripts dir to root level, setup.py installs all excecutable scripts to bin/

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
+
+# install all excecutable files under ./scripts to PREFIX/bin/
+import glob
+import os
+scripts = []
+for f in glob.glob('./scripts/*'):
+    if os.path.isfile(f) and os.access(f, os.X_OK):
+        scripts.append(f)
 
 setup(name='crane',
       version='0.0.1',
@@ -10,5 +17,6 @@ setup(name='crane',
       url='https://bitbucket.org/tkarna/crane',
       packages=['crane', 'crane.data', 'crane.plotting',
                 'crane.files'],
+      scripts=scripts,
       )
 
