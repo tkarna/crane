c80d91d10a30d28b35ad99fce923534704dfc44f
Tuomas Karna <tuomas.karna@gmail.com>
2016-01-08 15:38:06 -0800

    added add_colorbar and AxisLabeler to utility

--- crane/utility.py
+++ crane/utility.py
@@ -5,7 +5,8 @@ Tuomas Karna 2015-08-21
 """
 import datetime
 import os
-from crane import plt
+import string
+from crane import plt, matplotlib
 
 
 def parseTimeStr(timeStr):
@@ -89,3 +90,64 @@ def saveFigure(
         if verbose:
             print 'saving to', f
         plt.savefig(f, dpi=dpi, **kw)
+
+
+def add_colorbar(parent_ax, p, pos='left', label=None, pad=None, aspect=None):
+    parent_pos = parent_ax.get_position()
+    fig = parent_ax.get_figure()
+    parent_dx = parent_pos.x1 - parent_pos.x0
+    parent_dy = parent_pos.y1 - parent_pos.y0
+    vertical_pos = ['left', 'right']
+    if pos in vertical_pos:
+        orientation = 'vertical'
+        y0 = parent_pos.y0
+        dy = parent_dy
+        if aspect is None: 
+            aspect = 1.0/10.0
+        dx = aspect*dy
+        if pos == 'left':
+            if pad is None:
+                pad = -0.05*parent_dx
+            x0 = 1.0 + pad
+        else:
+            raise NotImplementedError('right colorbar not implemented yet')
+    else:
+        orientation = 'horizontal'
+        x0 = parent_pos.x0
+        dx = parent_dx
+        if aspect is None:
+            aspect = 1.0/20.0
+        dy = aspect*dx
+        if pos == 'bottom':
+            if pad is None:
+                pad = -0.12*parent_dy
+            y0 = 0.0 + pad
+        else:
+            raise NotImplementedError('top colorbar not implemented yet')
+    cb_ax = fig.add_axes((x0, y0, dx, dy))
+    cb = plt.colorbar(p, cax=cb_ax, format='%.1f', orientation=orientation)
+    if label:
+        cb.set_label(label)
+
+
+class AxisLabeler(object):
+    """
+    Adds identification characters such as a), b) etc to axes
+    """
+    def __init__(self, xloc=0.0, yloc=1.02, **kwargs):
+        self.kwargs = kwargs
+        self.kwargs.setdefault('horizontalalignment', 'left')
+        self.kwargs.setdefault('verticalalignment', 'bottom')
+        self.kwargs.setdefault('fontsizeincrement', 2)
+        self.fontsizeincrement = self.kwargs.pop('fontsizeincrement')
+        fontsize = matplotlib.rcParams['font.size']
+        self.kwargs.setdefault('fontsize', fontsize + self.fontsizeincrement)
+        self.xloc = xloc
+        self.yloc = yloc
+        self.char_iter = iter(string.ascii_lowercase)  # iterator for alphabets
+
+    def addlabel(self, ax, char=None):
+        if char is None:
+            char = self.char_iter.next()
+        ax.text(self.xloc, self.yloc, char + ')', transform=ax.transAxes,
+                **self.kwargs)
